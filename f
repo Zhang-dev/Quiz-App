be2a108104362bc363129455a0d108b775c8ceb3
Zhang-dev <55280516+Zhang-dev@users.noreply.github.com>
2019-10-14 14:32:29 +0200

    questions modified

---
+++ f
@@ -0,0 +1,11 @@
+[33mcommit c16426fb0f92e00a69234a793096fa866e194ba6[m[33m ([m[1;36mHEAD -> [m[1;32mmaster[m[33m)[m
+Author: Zhang-dev <55280516+Zhang-dev@users.noreply.github.com>
+Date:   Mon Oct 14 13:30:10 2019 +0200
+
+    first commit of Zhang
+
+[33mcommit dceeeb218d6e4591ca0f30ea4005898032db436e[m[33m ([m[1;31morigin/master[m[33m)[m
+Author: Zhang-dev <55280516+Zhang-dev@users.noreply.github.com>
+Date:   Tue Oct 8 17:58:38 2019 +0200
+
+    First commit. Create startup files.
--- js/Questionpackage.js
+++ js/Questionpackage.js
@@ -2,38 +2,61 @@ import Question from "./Question.js"
 export default class QuestionPackage {
   static getQuestions(anzahl) {
     let q1 = new Question(
-      "Wer ist der President1?",
-      ["Ich", "Du", "Keiner", "Jeder"],
+      "In order to create a string, we need to put the text inside...",
+      ["Quotation marks", "/ symbols", "<string></string> tag"],
       0
     );
     let q2 = new Question(
-      "Wer ist der President2?",
-      ["Ich", "Du", "Keiner", "Jeder"],
+      "A floating point number:",
+      ["is allowed to have a decimal place","is always positive", "is always smaller than 0"],
       0
     );
     let q3 = new Question(
-      "Wer ist der President3?",
-      ["Ich", "Du", "Keiner", "Jeder"],
+      "An object's properties are similar to variables; methods are similar to:",
+      ["functions", "properties", "conditionals", "operators"],
       0
     );
     let q4 = new Question(
-      "Wer ist der President4?",
-      ["Ich", "Du", "Keiner", "Jeder"],
+      "Whic of the following is not one of the new ES6 features?",
+      ["Hoisting", "Modules", "Object destructuring", "Template Literals"],
       0
     );
     let q5 = new Question(
-      "Wer ist der President5?",
-      ["Ich", "Du", "Keiner", "Jeder"],
+      `What sit the output of the following code?
+      const arr1 = [1,2,3,4,5];
+      const arr2 = [...arr1, 6];
+      const func = (...rest) => {
+        console.log(rest.length);
+        func(...arr1);
+        func(...arr2);
+      }`,
+      ["56", "rest", "65", "1"],
       0
     );
     let q6 = new Question(
-      "Wer ist der President6?",
-      ["Ich", "Du", "Keiner", "Jeder"],
+      `Waht is the output of this code?
+      function letItBe(){
+        if(true){
+          let v = 4;
+          console.log(v);
+        }
+        console.log(v);
+      }
+      letItBe()`,
+      ["4 2", "2 2", "2 4", "4 4"],
       0
     );
     let q7 = new Question(
-      "Wer ist der President7?",
-      ["Ich", "Du", "Keiner", "Jeder"],
+      `What is the output of this code?
+      const map = new Map();
+      map.set("one",1);
+      map.set("2","two");
+      if(map.has("two")){
+        console.log("two")
+      }else{
+        console.log(map.get("one"));
+      }`,
+      ["1", "Invalid Syntax", "true", "undefined"],
       0
     );
     let question_arr = [q1, q2, q3, q4, q5, q6, q7];
